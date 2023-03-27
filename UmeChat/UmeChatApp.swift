255c3e34e823e1ba8c89a822caa53634801049cb
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-27 18:31:00 +0530

    Initial Commit

---
+++ UmeChat/UmeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmeChatApp.swift
+//  UmeChat
+//
+//  Created by Sayor Debbarma on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UmeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
